ace72e9432432ac9e8dcf7d930cf3b2ed951d055
Gab-Serrano <gab.serrano@duocuc.cl>
2024-05-15 17:41:41 -0400

    #CORS - Auth

--- ms-auth/src/main/java/cl/govegan/msauth/auth/controller/AuthControllerConfig.java
+++ ms-auth/src/main/java/cl/govegan/msauth/auth/controller/AuthControllerConfig.java
@@ -1,4 +1,4 @@
-package cl.govegan.mssearchrecipe.controller;
+package cl.govegan.msauth.auth.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class SearchRecipeControllerConfig implements WebMvcConfigurer{
+public class AuthControllerConfig implements WebMvcConfigurer{
    
    @Override
    public void addCorsMappings(CorsRegistry registry) {
