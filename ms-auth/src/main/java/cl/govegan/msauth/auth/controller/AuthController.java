fa1829de13430090fe0e093893d3600a17e1f6a0
Gab-Serrano <gab.serrano@duocuc.cl>
2024-05-15 21:46:27 -0400

    Mejora login y registro + refreshtoken

--- ms-auth/src/main/java/cl/govegan/msauth/auth/controller/AuthController.java
+++ ms-auth/src/main/java/cl/govegan/msauth/auth/controller/AuthController.java
@@ -6,12 +6,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cl.govegan.msauth.auth.models.RefreshToken;
 import cl.govegan.msauth.auth.requests.LoginRequest;
 import cl.govegan.msauth.auth.requests.RegisterRequest;
+import cl.govegan.msauth.auth.requests.TokenRefreshRequest;
 import cl.govegan.msauth.auth.response.AuthResponse;
+import cl.govegan.msauth.auth.response.TokenRefreshResponse;
 import cl.govegan.msauth.auth.services.AuthService;
+import cl.govegan.msauth.auth.services.RefreshTokenService;
+import cl.govegan.msauth.exceptions.TokenRefreshException;
 import lombok.RequiredArgsConstructor;
-
 
 @RestController
 @RequestMapping("api/v1/auth")
@@ -19,18 +23,29 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
 
     private final AuthService authService;
-   
-   @PostMapping("/loginUser")
-   public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
-       
-       return ResponseEntity.ok(authService.loginUser(loginRequest));
-   }
-   
-   @PostMapping("/registerUser")
-   public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
-       
-       return ResponseEntity.ok(authService.registerUser(registerRequest));
+    private final RefreshTokenService refreshTokenService;
 
-   }
+    @PostMapping("/loginUser")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.loginUser(loginRequest));
+    }
 
+    @PostMapping("/registerUser")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.registerUser(registerRequest));
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<TokenRefreshResponse> refreshToken(@RequestBody TokenRefreshRequest request) {
+        String requestRefreshToken = request.getRefreshToken();
+
+        return refreshTokenService.findByToken(requestRefreshToken)
+                .map(refreshTokenService::verifyExpiration)
+                .map(RefreshToken::getUsername)
+                .map(username -> {
+                    String newAccessToken = authService.generateAccessToken(username);
+                    return ResponseEntity.ok(new TokenRefreshResponse(newAccessToken, requestRefreshToken));
+                })
+                .orElseThrow(() -> new TokenRefreshException(requestRefreshToken, "Refresh token is not in database!"));
+    }
 }
---
+++ ms-auth/src/main/java/cl/govegan/msauth/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package cl.govegan.msauth.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DuplicateUsernameException.class)
+    public ResponseStatusException handleDuplicateUsernameException(DuplicateUsernameException ex) {
+        return new ResponseStatusException(HttpStatus.CONFLICT, ex.getMessage());
+    }
+}
