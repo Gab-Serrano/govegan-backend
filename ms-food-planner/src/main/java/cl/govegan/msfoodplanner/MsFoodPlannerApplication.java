ac893ad9baa320b288b05d6ab03ee66ebabc1ae2
Gab-Serrano <gab.serrano@duocuc.cl>
2024-05-18 19:08:38 -0400

    #

---
+++ ms-food-planner/src/main/java/cl/govegan/msfoodplanner/MsFoodPlannerApplication.java
@@ -0,0 +1,13 @@
+package cl.govegan.msfoodplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFoodPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFoodPlannerApplication.class, args);
+	}
+
+}
